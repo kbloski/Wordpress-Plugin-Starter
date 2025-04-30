f8bf5bb2297e2350717aee82c2f3b91424787066
Kamil Blonski <kblonski02@gmail.com>
2025-04-30 11:58:59 +0400

    Add react to project

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import style from "./App.module.scss";
+import './App.css'
+
+function App() {
+  return (
+    <div className={style.App}>
+      Hello from react app
+    </div>
+
+  )
+}
+
+export default App
--- my-plugin.php
+++ my-plugin.php
@@ -17,3 +17,36 @@ register_activation_hook(__FILE__, 'Inc\InitPlugin::activate_plugin');
 register_deactivation_hook(__FILE__, 'Inc\InitPlugin::deactivate_plugin');
 
 InitPlugin::init();
+
+
+// Enqueue skryptów Reacta
+add_action('admin_enqueue_scripts', function () {
+    $asset_path = plugin_dir_url(__FILE__) . 'build/';
+
+    wp_enqueue_script('my-plugin-react', $asset_path . 'assets/index.js', [], null, true);
+    wp_enqueue_style('my-plugin-style', $asset_path . 'assets/index.css', [], null);
+    
+    // Można przekazać dane do Reacta
+    wp_localize_script('my-plugin-react', 'myPluginData', [
+        'ajaxUrl' => admin_url('admin-ajax.php'),
+        'nonce'   => wp_create_nonce('my-plugin-nonce'),
+    ]);
+});
+
+
+// Zakdka w menu
+add_action('admin_menu', function () {
+    add_menu_page(
+        'Alguin Plugin',            // Page Title
+        'Alguin Template 2025',     // Menu Title
+        'manage_options',           // Only Admin Can see this page
+        'alguin-plugin-template',   // slug strony - unikalny identyfikator uzywanyt w ury do identyfikacji strony
+        'my_plugin_render_page',    // Callback renderujący
+        'dashicons-admin-generic'   // Icon
+    );
+});
+
+function my_plugin_render_page() {
+    // Root element (Podłanczam do tego moją react app)
+    echo '<div id="my-plugin-admin-panel"></div>'; // kontener dla Reacta
+}
