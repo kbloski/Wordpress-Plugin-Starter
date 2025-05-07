7b5f9a6e533488c9fff2c98ade2f7c4b8490b44e
Kamil Blonski <kblonski02@gmail.com>
2025-05-07 10:23:12 +0400

    shortcode blog list

--- my-plugin.php
+++ my-plugin.php
@@ -25,3 +25,43 @@ ShortcodesReact::init();
 ApiManager::init();
 ScriptsManager::init();
 ShortcodesManager::init();
+
+
+add_shortcode('test-blog', 'custom_all_blog_posts_shortcode');
+
+function custom_all_blog_posts_shortcode() {
+    $query = new WP_Query( array(
+        'post_type'      => 'post',
+        'posts_per_page' => -1, // bez limitu
+        'post_status'    => 'publish',
+    ) );
+
+    ob_start();
+
+    if ( $query->have_posts() ) {
+        echo '<ul class="custom-blog-posts">';
+        while ( $query->have_posts() ) {
+            $query->the_post();
+
+             // Sprawdzanie, czy post ma obrazek wyróżniający
+            if ( has_post_thumbnail() ) {
+                // Wyświetlanie obrazka wyróżniającego (możesz dostosować rozmiar)
+                echo '<div class="post-thumbnail">';
+                echo get_the_post_thumbnail( get_the_ID(), 'thumbnail' ); // Zmien "thumbnail" na inny rozmiar, np. "medium", "large"
+                echo '</div>';
+            }
+
+            echo '<li>';
+            echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a>';
+            echo '<p>' . get_the_excerpt() . '</p>';
+            echo '</li>';
+        }
+        echo '</ul>';
+    } else {
+        echo '<p>Brak wpisów do wyświetlenia.</p>';
+    }
+
+    wp_reset_postdata();
+
+    return ob_get_clean();
+}
