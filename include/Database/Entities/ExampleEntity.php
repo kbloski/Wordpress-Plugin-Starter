aa7645af2e28c2e2594d2f7fa9613a0204e678d2
Kamil Blonski <kblonski02@gmail.com>
2025-05-14 12:21:12 +0400

    correction example entity

--- include/Database/Entities/ExampleEntity.php
+++ include/Database/Entities/ExampleEntity.php
@@ -8,14 +8,16 @@ class ExampleEntity
 {
     public static function createTable()
     {
+        global $wpdb;
         $table_name = TableNameHelper::getExampleTableName();
+        $charset_collate = $wpdb->get_charset_collate();
 
         $sql = "CREATE TABLE IF NOT EXISTS $table_name (
-            id INT(11) AUTO_INCREMENT PRIMARY KEY,
-            header VARCHAR(255) NOT NULL,
-            description TEXT NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )";
+            id INT(11) AUTO_INCREMENT PRIMARY KEY,  
+            header VARCHAR(255) NOT NULL,  
+            description TEXT NOT NULL,  
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP  
+        ) $charset_collate;";
 
         require_once(ABSPATH . "wp-admin/includes/upgrade.php");
         dbDelta($sql);
